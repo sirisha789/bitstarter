54ed5607d48796a688eb8aa69c3e37b93582b6ad
sirisha789 <sirisha.akkana@gmail.com>
2013-07-09 17:12:11 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
  var fs = require('fs');
  var content = fs.readFileSync('index.html','utf8');
- response.send(buf.toString());
+ response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
